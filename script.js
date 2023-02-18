e64e1ea2942ba5ee01c92df2b279d2b84bfd7c9b
Kyle Angelich <kyleangelich1@gmail.com>
2023-02-17 21:00:25 -0800

    Fix class vs. id errors causing display issues

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let gridContainer = document.querySelector("#grid-container");
 
-function createGrid () {
-    for (let i =0; i < 257; i++) {
+// function createGrid() {
+//     for (let i =0; i < 257; i++) {
         let divSquares = document.createElement("div");
-        gridContainer.appendChild(divSquares);
         divSquares.classList.add("divSquaresContent");
-        divSquares.style.color = black;
-        divSquares.style.border = solid;
-    }
-}
+        divSquares.textContent = "Test text";
+        divSquares.style.color = "black";
+        divSquares.style.border = "solid";
+        divSquares.style.backgroundColor = "blue";
+
+        gridContainer.appendChild(divSquares);
+//     }
+// }
