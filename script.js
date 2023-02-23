35d27802b590c91f6f97045855ecf4d3d049be0c
Kyle Angelich <kyleangelich1@gmail.com>
2023-02-22 16:54:06 -0800

    Add grid styling and remove 257th div

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 let gridContainer = document.querySelector("#grid-container");
 
 function createGrid() {
-    for (let i = 0; i < 257; i++) {
+    for (let i = 0; i < 256; i++) {
         let divSquares = document.createElement("div");
         divSquares.classList.add("divSquaresContent");
-        divSquares.textContent = "Test text";
-        divSquares.style.color = "black";
-        divSquares.style.border = "solid";
-        divSquares.style.backgroundColor = "blue";
+        divSquares.textContent = "Test";
 
         gridContainer.appendChild(divSquares);
     }
