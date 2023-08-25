9c4164adcb3604359876865c70ae225daa553e24
Kyle Angelich <kyleangelich1@gmail.com>
2023-08-24 17:26:24 -0700

    Add verification to confirm gridSize is a number

--- script.js
+++ script.js
@@ -58,7 +58,9 @@ const promptGridSize = () => {
     const gridValue = prompt("Please specify the desired grid size. E.g. 64 specifies a 64x64 grid.");
     const gridSize = parseInt(gridValue);
 
-    if (gridSize > 100) {
+    if (isNaN(gridSize)) {
+        alert("Please enter a valid number for the grid size.");
+    } else if (gridSize > 100) {
         alert("Exceeds maximum grid size of 100x100");
     } else {
         createGrid(gridSize);
