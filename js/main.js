3b8a556ddd14b7c0623bad0cc2f4f7275fbb03b9
Kyle Angelich <kyleangelich1@gmail.com>
2023-09-26 22:18:05 -0700

    Fix main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-import { promptGridSize, createGrid, clearGrid} from "./functionLibrary.js";
+import { promptGridSize, clearGrid } from "./functionLibrary.js";
 
 const promptButton = document.getElementById("prompt-button");
 const gridContainer = document.querySelector("#grid-container");
 const clearButton = document.getElementById("clear-button");
 
-promptButton.addEventListener("click", promptGridSize);
-clearButton.addEventListener("click", clearGrid);
+promptButton.addEventListener("click", () => promptGridSize(gridContainer));
+clearButton.addEventListener("click", () => clearGrid(gridContainer));
